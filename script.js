6c5aa07a4d0692e01458d98d0108cb6b77342d88
HadiltonSelis <hadilton@outlook.com>
2025-12-01 15:46:38 -0300

    Atualiza lógica no script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+//Soma de dois números:
+
+function soma(a, b) {
+    return a + b;
+}
+
+console.log(soma(2,7));
+
+//Par ou ímpar:
+
+function parOuImpar(n) {
+    
+    return n % 2 === 0 ? "par" : "Ímpar";
+
+}
+
+console.log(parOuImpar(11));
